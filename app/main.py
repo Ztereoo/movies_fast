6ec9c7b795a4813e5fd6b036376aa9a2c5fdf2d6
stereo <airstarview@gmail.com>
2025-01-22 15:36:55 +0300

    Dependencies for User added

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import uvicorn
 from fastapi import FastAPI,Depends
 from app.movies.router import router as router_movies
 from app.users.router import router as router_users
+from app.reviews.router import router as router_reviews
 from app.users.models import User
 from app.users.dependencies import get_current_user
 from dao.base import BaseDao
@@ -11,10 +12,11 @@ app= FastAPI()
 
 app.include_router(router_movies)
 app.include_router(router_users)
+app.include_router(router_reviews)
 
-@app.get('/rev')
-async def get_reviews(user: User= Depends(get_current_user)):
-    print(user, type(user), user.email)
+# @app.get('/rev')
+# async def get_reviews(user: User= Depends(get_current_user)):
+#     print(user, type(user), user.email)
 
 
 
