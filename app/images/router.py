879c52b862926f467cc5e63de038f28744cf835d
stereo <airstarview@gmail.com>
2025-01-23 14:14:39 +0300

    Celery task created celery moved to app

--- app/images/router.py
+++ app/images/router.py
@@ -1,5 +1,6 @@
 from fastapi import UploadFile, APIRouter
 import shutil
+from app.tasks.tasks import resize_pic
 
 router = APIRouter(
     prefix='/images',
@@ -13,5 +14,7 @@ async def add_movies_images(file: UploadFile, name: int | None = None):
         original_filename = name
     else:
         original_filename = file.filename.split('.')[0]
-    with open(f'app/static/images/{original_filename}.webp', "wb+") as file_object:
+    im_path= f'app/static/images/{original_filename}.webp'
+    with open(im_path, "wb+") as file_object:
         shutil.copyfileobj(file.file, file_object)
+    resize_pic.delay(im_path)
---
+++ app/tasks/tasks.py
@@ -0,0 +1,14 @@
+from app.tasks.celery import celery
+from PIL import Image
+from pathlib import Path
+@celery.task
+def resize_pic(
+        path: str,
+):
+    im_path= Path(path)
+    img= Image.open(im_path)
+    resized_1000_500= img.resize(1000,600)
+    resized_200_100= img.resize(200,100)
+    resized_1000_500.save(f'app/static/images/{im_path.name}_resized_1000_600')
+    resized_200_100.save(f'app/static/images/{im_path.name}_resized_200_100')
+
