14fd4f47620af4e415dc53f22b473bff1d2d9a85
stereo <airstarview@gmail.com>
2025-02-07 20:03:50 +0300

    docker compose before bash scripts

--- config.py
+++ config.py
@@ -1,12 +1,15 @@
 import os
+from dotenv import load_dotenv
 from typing import Literal
 
-KEY= 'asdlajsdasASDASD'
-ALGORITHM='HS256'
+load_dotenv()
 
-SMTP_HOST= 'smtp.gmail.com'
-SMTP_PORT= 465
-SMTP_USER= 'airstarview@gmail.com'
-SMTP_PASS='guma lbnl pmgc uhqc'
+KEY = os.getenv("KEY", "default_key")
+ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
-MODE: Literal['DEV', 'TEST', 'PROD'] = os.getenv('MODE', 'DEV')
+SMTP_HOST = os.getenv("SMTP_HOST", "smtp.gmail.com")
+SMTP_PORT = int(os.getenv("SMTP_PORT", 465))
+SMTP_USER = os.getenv("SMTP_USER", "default@gmail.com")
+SMTP_PASS = os.getenv("SMTP_PASS", "default_password")
+
+MODE: Literal['DEV', 'TEST', 'PROD'] = os.getenv("MODE", "DEV")
