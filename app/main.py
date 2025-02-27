6d87804d8d4511781fbf314e7448b17a7c1bf328
stereo <airstarview@gmail.com>
2025-02-27 13:52:03 +0300

    logger function added to main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
+import time
 from contextlib import asynccontextmanager
+from datetime import datetime
 
 import uvicorn
 from app.database import engine
@@ -8,6 +10,7 @@ from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from redis import asyncio as aioredis
 from sqladmin import Admin
+from app.logger import logger
 
 from app.admin.views import MovieAdmin, ReviewAdmin, UserAdmin
 from app.images.router import router as router_images
@@ -24,10 +27,10 @@ async def lifespan(app: FastAPI):
     yield
     await redis.close()
 
+
 app = FastAPI(lifespan=lifespan)
 
-admin=Admin(app,engine)
-
+admin = Admin(app, engine)
 
 admin.add_view(UserAdmin)
 admin.add_view(MovieAdmin)
@@ -35,11 +38,22 @@ admin.add_view(ReviewAdmin)
 
 app.mount("/static", StaticFiles(directory="app/static"), "static")
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    process_time = time.time() - start_time
+    logger.info("Request execution time", extra={
+        "process_time": round(process_time, 4)
+    })
+    return response
+
+
 app.include_router(router_movies)
 app.include_router(router_users)
 app.include_router(router_reviews)
 app.include_router(router_images)
-
 
 # if __name__ == "__main__":
 #     uvicorn.run(app)
