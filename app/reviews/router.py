973c3c637d01f7bb93d6adcc0d6084e3c8a6f2fc
stereo <airstarview@gmail.com>
2025-03-16 20:12:23 +0300

    reviews routers changed for correct working with session via Depends func

--- app/reviews/router.py
+++ app/reviews/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, Depends
 
+from sqlalchemy.ext.asyncio import AsyncSession
 from app.reviews.dao import ReviewDao
 from app.users.dependencies import get_current_user
 from app.users.models import User
+from app.database import get_session
 
 router = APIRouter(
     prefix="/reviews",
@@ -11,10 +13,11 @@ router = APIRouter(
 
 
 @router.get("/all_reviews")
-async def get_all_reviews():
-    return await ReviewDao.find_all()
+async def get_all_reviews(session: AsyncSession = Depends(get_session)):
+    return await ReviewDao.find_all(session)
 
 
 @router.get("/get_users_reviews")
-async def get_users_reviews(user: User = Depends(get_current_user)):
-    return await ReviewDao.find_all(user_id=user.id)
+async def get_users_reviews(user: User = Depends(get_current_user),
+                            session: AsyncSession = Depends(get_session)):
+    return await ReviewDao.find_all(session,user_id=user.id)
--- dao/base.py
+++ dao/base.py
@@ -74,7 +74,7 @@ class BaseDao:
         item = await session.execute(stmt)
         item = item.scalars().one_or_none()
         if item:
-                await session.delete(item)
-                await session.commit()
-                return f"Item deleted"
+            await session.delete(item)
+            await session.commit()
+            return f"Item deleted"
         return HTTPException(status_code=400, detail="no item with such id")
